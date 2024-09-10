984de19c32b3c89c5f5d355782a1983d2af5141d
elis-elis <elisprostotak@gmail.com>
2024-09-10 13:12:53 +0200

    add 404 and 500 error.handlers in app.py, create 404.html and 500.heml, modify delete_movie.html and delete_user.html

--- app.py
+++ app.py
@@ -273,5 +273,15 @@ def delete_user(user_id):
     return redirect(url_for('list_users'))
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True)
