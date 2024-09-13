724242352789f5900bcab23679172b13ada75dbd
elis-elis <elisprostotak@gmail.com>
2024-09-13 19:29:31 +0200

    update logging_config.py file

--- config/logging_config.py
+++ config/logging_config.py
@@ -5,13 +5,18 @@ import os
 
 def setup_logging(app: Flask):
     if not app.debug:
-        # Ensure the 'logs' directory exists
-        if not os.path.exists('logs'):
-            os.makedirs('logs')
+        log_dir = os.path.join(os.path.expanduser("~"), 'moviweb_app', 'logs')
 
-        # File handler for logging to 'logs/error.log'
-        file_handler = logging.FileHandler('logs/error.log')  # Update this line
+        # Ensure the 'logs' directory exists
+        if not os.path.exists(log_dir):
+            os.makedirs(log_dir, exist_ok=True)
+            # exist_ok=True: Ensures the directory creation doesn't raise an error if it already exists
+
+        # File handler for logging to 'error.log' in the writable logs directory
+        log_file_path = os.path.join(log_dir, 'error.log')
+        file_handler = logging.FileHandler(log_file_path)
         file_handler.setLevel(logging.ERROR)
+
         formatter = logging.Formatter(
             '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'
         )
